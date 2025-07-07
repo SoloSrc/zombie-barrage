bb8eb98859e8dac7137c3fa86b8b85b7f0f4d7be
Vítor de Albuquerque Torreão <vitorreao@pm.me>
2025-07-06 21:50:45 -0300

    Change to KayKit

--- scripts/character_controller.gd
+++ scripts/character_controller.gd
@@ -9,7 +9,6 @@ const JUMP_VELOCITY = 4.5
 @onready var animation_tree: AnimationTree = $AnimationTree
 
 func _physics_process(delta: float) -> void:
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction: Vector3 = Vector3.ZERO
@@ -23,6 +22,7 @@ func _physics_process(delta: float) -> void:
 		direction.x += 1
 	
 	if direction != Vector3.ZERO:
+		animation_tree.set("parameters/Movement/blend_amount", 1.0)
 		direction = direction.normalized()
 		look_at(global_position + direction)
 		velocity.x = direction.x * SPEED
@@ -30,21 +30,16 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
+		animation_tree.set("parameters/Movement/blend_amount", 0)
 	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	
-	var ground_velocity: Vector2 = Vector2(velocity.x, velocity.z)
-	if ground_velocity.length() > 0 and get_anim_state() != PISTOL_RUN_STATE:
-		set_anim_state(PISTOL_RUN_STATE)
-	elif ground_velocity.length() == 0 and get_anim_state() != IDLE_STATE:
-		set_anim_state(IDLE_STATE)
-	
 	move_and_slide()
 
 func set_anim_state(state: String) -> void:
-	animation_tree.set("parameters/state/transition_request", state)
+	animation_tree.set("parameters/Movement/blend_amount", state)
 
 func get_anim_state() -> String:
 	return animation_tree.get("parameters/state/current_state")
