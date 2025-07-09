93d785f8b3e3e0d8b4c479d7d3b375a66d66d2dd
Vítor de Albuquerque Torreão <vitorreao@pm.me>
2025-07-09 15:24:52 -0300

    Player death

--- scripts/player_character.gd
+++ scripts/player_character.gd
@@ -1,6 +1,8 @@
 extends CharacterBody3D
 class_name PlayerCharacter
 
+signal player_death(player: PlayerCharacter)
+
 const IDLE_STATE = "idle"
 const PISTOL_RUN_STATE = "pistol_run"
 
@@ -12,6 +14,18 @@ const HEALTH = 500.0
 @onready var animation_tree: AnimationTree = $AnimationTree
 
 func _physics_process(delta: float) -> void:
+	if health >= 0.0:
+		calc_movement()
+	elif animation_tree.get("parameters/Transition/current_state") != "Dead":
+		animation_tree.set("parameters/Transition/transition_request", "Dead")
+	
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	
+	move_and_slide()
+
+func calc_movement() -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction: Vector3 = Vector3.ZERO
@@ -21,23 +35,17 @@ func _physics_process(delta: float) -> void:
 	direction = direction.normalized()
 	
 	if direction != Vector3.ZERO:
-		animation_tree.set("parameters/Movement/blend_amount", 1.0)
+		set_mov_blend_amount(1.0)
 		look_at(global_position + direction)
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-		animation_tree.set("parameters/Movement/blend_amount", 0)
-	
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-	
-	move_and_slide()
+		set_mov_blend_amount(0)
 
-func set_anim_state(state: String) -> void:
-	animation_tree.set("parameters/Movement/blend_amount", state)
+func set_mov_blend_amount(amt: float) -> void:
+	animation_tree.set("parameters/Movement/blend_amount", amt)
 
 func get_anim_state() -> String:
 	return animation_tree.get("parameters/state/current_state")
